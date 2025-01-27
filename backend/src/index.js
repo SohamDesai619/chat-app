41169857c175e65c80e6440c48854eb66ff47ad6
SohamDesai619 <sohamd619@gmail.com>
2025-01-27 23:15:34 +0530

    last commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,41 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import express from "express"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import path from "path"
+import path from "path";
 
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js";
-import { app,server } from "./lib/socket.js"
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
 dotenv.config();
 
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-
-const PORT = process.env.PORT 
-const __dirname = path.resolve()
-
-app.use(express.json())  // to parse json request bodies
-
-app.use(cookieParser())
-app.use(cors({
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true, // Allow cookies to be sent
-}));
+    credentials: true,
+  })
+);
 
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-server.listen(PORT,()=>{
-    console.log("Server running on PORT:"+PORT);
-    connectDB()
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
